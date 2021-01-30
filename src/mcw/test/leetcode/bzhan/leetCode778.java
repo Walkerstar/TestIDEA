f343674ad096710af522fc71d4214a4cf4a76fef
Walkerstar <56328935+Walkerstar@users.noreply.github.com>
2021-01-30 20:19:32 +0800

    新增一题

--- src/mcw/test/leetcode/bzhan/leetCode778.java
+++ src/mcw/test/leetcode/bzhan/leetCode778.java
@@ -12,6 +12,9 @@ import java.util.*;
  */
 public class leetCode778 {
 
+    /**
+     * 并查集
+     */
     public int swimInWater(int[][] grid) {
         int m = grid.length;
         int n = grid[0].length;
@@ -39,6 +42,124 @@ public class leetCode778 {
             }
         }
         return ans ;
+    }
+
+    /**
+     * 二分查找 + 深度优先/广度优先
+     */
+    int[][] directions = {{0, 1}, {0, -1}, {1, 0}, {-1, 0}};
+
+    static int n;
+
+    public int swimInWater1(int[][] grid) {
+        n = grid.length;
+        int left = 0;
+        int right = n * n - 1;
+        while (left < right) {
+            //left + right 不会溢出
+            int mid = (left + right) / 2;
+            boolean[][] visited = new boolean[n][n];
+            if (grid[0][0] <= mid && dfs(grid, 0, 0, visited, mid)/*或者 bfs(grid, mid))*/) {
+                // mid 可以，尝试 mid 小一点是不是也可以呢？下一轮搜索的区间 [left, mid]
+                right = mid;
+            } else {
+                left = mid + 1;
+            }
+        }
+        return left;
+    }
+
+    /**
+     * 使用深度优先遍历得到从 (x, y) 开始向四个方向的所有小于等于 threshold 且与 (x, y) 连通的结点
+     */
+    private boolean dfs(int[][] grid, int x, int y, boolean[][] visited, int threshold) {
+        visited[x][y] = true;
+        for (int[] direction : directions) {
+            int X = x + direction[0];
+            int Y = y + direction[1];
+            if (inArea(X, Y) && !visited[X][Y] && grid[X][Y] <= threshold) {
+                if (X == n - 1 && Y == n - 1) {
+                    return true;
+                }
+                if (dfs(grid, X, Y, visited, threshold)) {
+                    return true;
+                }
+            }
+        }
+        return false;
+    }
+
+    /**
+     * 使用广度优先遍历得到从 (x, y) 开始向四个方向的所有小于等于 threshold 且与 (x, y) 连通的结点
+     */
+    private boolean bfs(int[][] grid, int threshold) {
+        Queue<int[]> queue = new LinkedList<>();
+        queue.offer(new int[]{0, 0});
+        boolean[][] visited = new boolean[n][n];
+        visited[0][0] = true;
+        while (!queue.isEmpty()) {
+            int[] front = queue.poll();
+            int x = front[0];
+            int y = front[1];
+            for (int[] direction : directions) {
+                int X = x + direction[0];
+                int Y = y + direction[1];
+                if (inArea(X, Y) && !visited[X][Y] && grid[X][Y] <= threshold) {
+                    if (X == n - 1 && Y == n - 1) {
+                        return true;
+                    }
+                    queue.offer(new int[]{X, Y});
+                    visited[X][Y] = true;
+                }
+            }
+        }
+        return false;
+    }
+
+    private boolean inArea(int x, int y) {
+        return x >= 0 && x < n && y >= 0 && y < n;
+    }
+
+    /**
+     * Dijkstra 算法( 前提: 没有负权边，找单源最短路径)
+     */
+    public int swimInWater2(int[][] grid) {
+        int n = grid.length;
+        Queue<int[]> minHeap = new PriorityQueue<>(Comparator.comparingInt(o -> grid[0][o[1]]));
+        minHeap.offer(new int[]{0, 0});
+        boolean[][] visited = new boolean[n][n];
+        //distTo[i][j]表示: 到顶点[i,j]需要等待的最少时间
+        int[][] distTo = new int[n][n];
+        for (int[] to : distTo) {
+            Arrays.fill(to, n * n);
+        }
+        distTo[0][0] = grid[0][0];
+        while (!minHeap.isEmpty()) {
+            //找最短的边
+            int[] front = minHeap.poll();
+            int currentX = front[0];
+            int currentY = front[1];
+            if (visited[currentX][currentY]) {
+                continue;
+            }
+            //确定最短路径顶点
+            visited[currentX][currentY] = true;
+            if (currentX == n - 1 && currentY == n - 1) {
+                return distTo[n - 1][n - 1];
+            }
+
+            //更新
+            for (int[] direction : directions) {
+                int newX = currentX + direction[0];
+                int newY = currentY + direction[1];
+                if (inArea(newX, newY) && !visited[newX][newY] &&
+                        Math.max(distTo[currentX][currentY], grid[newX][newY]) < distTo[newX][newY]) {
+                    distTo[newX][newY] = Math.max(distTo[currentX][currentY], grid[newX][newY]);
+                    minHeap.offer(new int[]{newX, newY});
+                }
+            }
+        }
+        return -1;
     }
 
     class UnionFind {
