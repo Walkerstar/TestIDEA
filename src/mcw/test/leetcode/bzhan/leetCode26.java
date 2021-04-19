be4622458e9261352418d432bba5490b2e99ab5d
Walkerstar <1342749460@qq.com>
2021-04-19 10:42:25 +0800

    新增一题

--- src/mcw/test/leetcode/bzhan/leetCode26.java
+++ src/mcw/test/leetcode/bzhan/leetCode26.java
@@ -1,9 +1,9 @@
 package mcw.test.leetcode.bzhan;
 
 /**
- * @author mcw 2020\5\11 0011-15:13
  * remove duplicates from sorted array
  * 删除重复的元素，返回剩余的元素个数
+ * @author mcw 2020\5\11 0011-15:13
  */
 public class leetCode26 {
     public static int removeDuplicates(int[] nums){
@@ -12,6 +12,25 @@ public class leetCode26 {
             if(nums[i-1]!=nums[i]){
                 nums[slow++]=nums[i];
             }
+        }
+        return slow;
+    }
+
+    /**
+     * 双指针
+     */
+    public int removeDuplicates1(int[] nums) {
+        int n = nums.length;
+        if (n == 0) {
+            return 0;
+        }
+        int fast = 1, slow = 1;
+        while (fast < n) {
+            if (nums[fast] != nums[fast - 1]) {
+                nums[slow] = nums[fast];
+                ++slow;
+            }
+            ++fast;
         }
         return slow;
     }
--- src/mcw/test/leetcode/bzhan/leetCode27.java
+++ src/mcw/test/leetcode/bzhan/leetCode27.java
@@ -1,8 +1,8 @@
 package mcw.test.leetcode.bzhan;
 
 /**
- * @author mcw 2020\5\4 0004-15:54
  * 删除指定元素，返回剩下的有效数字的个数
+ * @author mcw 2020\5\4 0004-15:54
  */
 public class leetCode27 {
     public static int removeElement(int[] nums,int val){
@@ -14,6 +14,30 @@ public class leetCode27 {
             }
         }
         return slow;
+    }
+
+    /**
+     * 优化双指针
+     * 
+     * 如果左指针 left 指向的元素等于 val，此时将右指针 right 指向的元素复制到左指针 left 的位置，然后右指针 right 左移一位。
+     * 如果赋值过来的元素恰好也等于 val，可以继续把右指针 right 指向的元素的值赋值过来（左指针 left 指向的等于 val 的元素的
+     * 位置继续被覆盖），直到左指针指向的元素的值不等于 val 为止。
+     *
+     * 当左指针 left 和右指针 right 重合的时候，左右指针遍历完数组中所有的元素。
+     *
+     */
+    public int removeElement1(int[] nums,int val){
+        int left=0;
+        int right= nums.length;
+        while (left<right){
+            if (nums[left]==val){
+                nums[left]=nums[right-1];
+                right--;
+            }else {
+                left++;
+            }
+        }
+        return left;
     }
 
     public static void main(String[] args) {
