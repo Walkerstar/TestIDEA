a63e14f4fa67eda6207a4cb398b7be0e20a388a1
Walkerstar <1342749460@qq.com>
2021-04-26 15:34:29 +0800

    新增一题

---
+++ src/mcw/test/leetcode/bzhan/leetCode1011.java
@@ -0,0 +1,40 @@
+package mcw.test.leetcode.bzhan;
+
+import java.util.Arrays;
+
+/**
+ * 传送带上的包裹必须在 D 天内从一个港口运送到另一个港口。
+ * <p>
+ * 传送带上的第 i 个包裹的重量为 weights[i]。每一天，我们都会按给出重量的顺序往传送带上装载包裹。我们装载的重量不会超过船的最大运载重量。
+ * <p>
+ * 返回能在 D 天内将传送带上的所有包裹送达的船的最低运载能力。
+ *
+ * @author mcw 2021\4\26 0026-15:23
+ */
+public class leetCode1011 {
+
+    public int shipWithinDays(int[] weights, int D) {
+        //确定二分查找左右边界
+        int left = Arrays.stream(weights).max().getAsInt();
+        int right = Arrays.stream(weights).sum();
+        while (left < right) {
+            int mid = (left + right) / 2;
+            //need 为需要运送的天数
+            //cur 为当前这一天已经运送的包裹重量之和
+            int need = 1, cur = 0;
+            for (int weight : weights) {
+                if (cur + weight > mid) {
+                    ++need;
+                    cur = 0;
+                }
+                cur += weight;
+            }
+            if (need <= D) {
+                right = mid;
+            } else {
+                left = mid + 1;
+            }
+        }
+        return left;
+    }
+}
---
+++ src/mcw/test/leetcode/bzhan/leetCode897.java
@@ -0,0 +1,58 @@
+package mcw.test.leetcode.bzhan;
+
+import mcw.test.common.TreeNode;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * 给你一棵二叉搜索树，请你 按中序遍历 将其重新排列为一棵递增顺序搜索树，使树中最左边的节点成为树的根节点，并且每个节点没有左子节点，只有一个右子节点。
+ *
+ * @author mcw 2021\4\25 0025-15:10
+ */
+public class leetCode897 {
+
+    public TreeNode increasingBST(TreeNode root) {
+        List<Integer> res = new ArrayList<>();
+        inorder(root, res);
+        TreeNode dummyNode = new TreeNode(-1);
+        TreeNode currNode = dummyNode;
+        for (int value : res) {
+            currNode.right = new TreeNode(value);
+            currNode = currNode.right;
+        }
+        return dummyNode.right;
+    }
+
+    private void inorder(TreeNode node, List<Integer> res) {
+        if (node == null) {
+            return;
+        }
+        inorder(node.left, res);
+        res.add(node.val);
+        inorder(node.right, res);
+    }
+
+
+    private TreeNode resNode;
+
+    public TreeNode increasingBST1(TreeNode root) {
+        TreeNode dummyNode = new TreeNode(-1);
+        resNode = dummyNode;
+        inorder(root);
+        return dummyNode.right;
+    }
+
+    public void inorder(TreeNode node) {
+        if (node == null) {
+            return;
+        }
+        inorder(node.left);
+        //在中序遍历的过程中修改节点指向
+        resNode.right = node;
+        node.left = null;
+        resNode = node;
+
+        inorder(node.right);
+    }
+}
