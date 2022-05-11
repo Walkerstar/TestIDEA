42bebc67072477a60926f07d5bb9ded147c9feb8
mcw <1342749460@qq.com>
2022-05-11 16:36:52 +0800

    新增一题

---
+++ src/mcw/test/leetcode/bzhan/leetCode449.java
@@ -0,0 +1,78 @@
+package mcw.test.leetcode.bzhan;
+
+import mcw.test.common.TreeNode;
+
+import java.util.ArrayDeque;
+import java.util.ArrayList;
+import java.util.Deque;
+import java.util.List;
+
+/**
+ * 序列化是将数据结构或对象转换为一系列位的过程，以便它可以存储在文件或内存缓冲区中，或通过网络连接链路传输，
+ * 以便稍后在同一个或另一个计算机环境中重建。
+ * <p>
+ * 设计一个算法来序列化和反序列化 二叉搜索树 。 对序列化/反序列化算法的工作方式没有限制。 您只需确保二叉搜索树可以序列化为字符串，
+ * 并且可以将该字符串反序列化为最初的二叉搜索树。
+ * <p>
+ * 编码的字符串应尽可能紧凑。
+ *
+ * @author mcw 2022/5/11 16:01
+ */
+public class leetCode449 {
+
+    /**
+     * 序列化
+     */
+    public String serialize(TreeNode root) {
+        List<Integer> list = new ArrayList<>();
+        postOrder(root, list);
+        String str = list.toString();
+        return str.substring(1, str.length() - 1);
+    }
+
+    /**
+     * 反序列化
+     */
+    public TreeNode deserialize(String data) {
+        if (data.isEmpty()) {
+            return null;
+        }
+        String[] arr = data.split(",");
+        Deque<Integer> stack = new ArrayDeque<>();
+        int length = arr.length;
+        for (int i = 0; i < length; i++) {
+            stack.push(Integer.parseInt(arr[i]));
+        }
+        return construct(Integer.MIN_VALUE, Integer.MAX_VALUE, stack);
+    }
+
+    /**
+     * 后序遍历
+     */
+    private void postOrder(TreeNode root, List<Integer> list) {
+        if (root == null) {
+            return;
+        }
+        postOrder(root.left, list);
+        postOrder(root.right, list);
+        list.add(root.val);
+    }
+
+    /**
+     * 后序遍历得到的数组中，
+     * 根结点的值位于数组末尾，
+     * 左子树的节点均小于根节点的值，
+     * 子树的节点均大于根节点的值，
+     */
+    private TreeNode construct(int lower, int upper, Deque<Integer> stack) {
+        if (stack.isEmpty() || stack.peek() < lower || stack.peek() > upper) {
+            return null;
+        }
+        int val = stack.pop();
+        TreeNode root = new TreeNode(val);
+        root.right = construct(val, upper, stack);
+        root.left = construct(lower, val, stack);
+        return root;
+    }
+
+}
