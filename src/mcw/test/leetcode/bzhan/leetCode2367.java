c7a314841d39879776ae1bb36bf7b93b5d87ebac
mcw <1342749460@qq.com>
2023-03-31 11:06:18 +0800

    新增一题

---
+++ src/mcw/test/leetcode/bzhan/leetCode2367.java
@@ -0,0 +1,95 @@
+package mcw.test.leetcode.bzhan;
+
+import java.util.HashSet;
+import java.util.Set;
+
+/**
+ * 给你一个下标从 0 开始、严格递增 的整数数组 nums 和一个正整数 diff 。如果满足下述全部条件，则三元组 (i, j, k) 就是一个 算术三元组 ：
+ * <p>
+ * i < j < k ，
+ * nums[j] - nums[i] == diff 且
+ * nums[k] - nums[j] == diff
+ * 返回不同 算术三元组 的数目。
+ * <p>
+ * 示例 1：
+ * <p>
+ * 输入：nums = [0,1,4,6,7,10], diff = 3
+ * 输出：2
+ * 解释：
+ * (1, 2, 4) 是算术三元组：7 - 4 == 3 且 4 - 1 == 3 。
+ * (2, 4, 5) 是算术三元组：10 - 7 == 3 且 7 - 4 == 3 。
+ * <p>
+ * 提示：
+ * <p>
+ * 3 <= nums.length <= 200
+ * 0 <= nums[i] <= 200
+ * 1 <= diff <= 50
+ * nums 严格 递增
+ *
+ * @author mcw 2023/3/31 10:38
+ */
+public class leetCode2367 {
+
+    /**
+     * 暴力枚举
+     */
+    public int arithmeticTriplets(int[] nums, int diff) {
+        int ans = 0;
+        int n = nums.length;
+        for (int i = 0; i < n; i++) {
+            for (int j = i + 1; j < n; j++) {
+                if (nums[j] - nums[i] != diff) {
+                    continue;
+                }
+                for (int k = j + 1; k < n; k++) {
+                    if (nums[k] - nums[j] == diff) {
+                        ans++;
+                    }
+                }
+            }
+        }
+        return ans;
+    }
+
+    /**
+     * 哈希集合
+     */
+    public int arithmeticTriplets2(int[] nums, int diff) {
+        Set<Integer> set = new HashSet<>();
+        for (int x : nums) {
+            set.add(x);
+        }
+        int ans = 0;
+        for (int x : nums) {
+            if (set.contains(x + diff) && set.contains(x + 2 * diff)) {
+                ans++;
+            }
+        }
+        return ans;
+    }
+
+    /**
+     * 三指针
+     */
+    public int arithmeticTriplets3(int[] nums, int diff) {
+        int ans = 0;
+        int n = nums.length;
+        for (int i = 0, j = 1, k = 2; i < n - 2 && j < n - 1 && k < n; i++) {
+            j = Math.max(j, i + 1);
+            while (j < n - 1 && nums[j] - nums[i] < diff) {
+                j++;
+            }
+            if (j >= n - 1 || nums[j] - nums[i] > diff) {
+                continue;
+            }
+            k = Math.max(k, j + 1);
+            while (k < n && nums[k] - nums[j] < diff) {
+                k++;
+            }
+            if (k < n && nums[k] - nums[j] == diff) {
+                ans++;
+            }
+        }
+        return ans;
+    }
+}
