a57f305b50a12c06a4bf41a21f347aa766add337
Walkerstar <1342749460@qq.com>
2023-05-29 10:33:36 +0800

    新增一题

--- src/mcw/test/leetcode/bzhan/leetCode28.java
+++ src/mcw/test/leetcode/bzhan/leetCode28.java
@@ -3,8 +3,9 @@ package mcw.test.leetcode.bzhan;
 /**
  * implement strStr()
  * 字符串匹配，返回子串在母串中的索引位置
+ * <p>
+ * 给你两个字符串 haystack 和 needle ，请你在 haystack 字符串中找出 needle 字符串出现的第一个位置（下标从 0 开始）。如果不存在，则返回 -1 。
  *
- * 给你两个字符串 haystack 和 needle ，请你在 haystack 字符串中找出 needle 字符串出现的第一个位置（下标从 0 开始）。如果不存在，则返回  -1 。
  * @author mcw 2020\5\11 0011-15:47
  */
 public class leetCode28 {
@@ -33,31 +34,31 @@ public class leetCode28 {
     /**
      * KMP 算法
      */
-    public int strStr2(String haystack,String needle){
+    public int strStr2(String haystack, String needle) {
         int n = haystack.length(), m = needle.length();
         if (m == 0) {
             return 0;
         }
-        int[] pi=new int[m];
-        for (int i = 0,j=0; i < m; i++) {
-            while (j>0 && needle.charAt(i)!=needle.charAt(j)){
-                j=pi[j-1];
+        int[] pi = new int[m];
+        for (int i = 0, j = 0; i < m; i++) {
+            while (j > 0 && needle.charAt(i) != needle.charAt(j)) {
+                j = pi[j - 1];
             }
-            if (needle.charAt(i)==needle.charAt(j)){
+            if (needle.charAt(i) == needle.charAt(j)) {
                 j++;
             }
-            pi[i]=j;
+            pi[i] = j;
         }
 
-        for (int i = 0,j=0; i < n; i++) {
-            while (j>0 && haystack.charAt(i)!=needle.charAt(j)){
-                j=pi[j-1];
+        for (int i = 0, j = 0; i < n; i++) {
+            while (j > 0 && haystack.charAt(i) != needle.charAt(j)) {
+                j = pi[j - 1];
             }
-            if (haystack.charAt(i)==needle.charAt(j)){
+            if (haystack.charAt(i) == needle.charAt(j)) {
                 j++;
             }
-            if (j==m){
-                return i-m+1;
+            if (j == m) {
+                return i - m + 1;
             }
         }
         return -1;
@@ -67,7 +68,7 @@ public class leetCode28 {
      * 错误
      */
     public static int strStr(String haystack, String needle) {
-        if ((needle==null || "".equals(needle)) && (haystack==null || "".equals(haystack))) return 0;
+        if ((needle == null || "".equals(needle)) && (haystack == null || "".equals(haystack))) return 0;
         if (needle == null || needle.length() == 0) return -1;
         if (haystack == null || haystack.length() == 0) return -1;
         int i = 0;
